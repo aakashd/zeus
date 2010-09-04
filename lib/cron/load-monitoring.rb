2fed84b69e487bb6cc85e72308d3b91f13961d75
aakash <aakashd@gmail.com>
2010-09-04 13:22:05 +0530

    bug fix

--- lib/cron/load-monitoring.rb
+++ lib/cron/load-monitoring.rb
@@ -27,6 +27,7 @@ if Zeus.is_admin
     logger.debug("starting another 1 instance as avg_queue is more than 10")
     instances = Zeus.ec2.run_instances(Zeus.ami,1,1,[Zeus.ec2_config['security_group']],Zeus.ec2_config['key'])
     new_instance_ids = instances.collect{|i| i[:aws_instance_id] }
+    logger.debug("new instance ids - #{new_instance_ids}")
     Zeus.elb.register_instances_with_load_balancer(Zeus.ec2_config['elb-name'], *instance_ids)
 
   elsif !avg_queue.nil? && avg_queue.to_f == 0
